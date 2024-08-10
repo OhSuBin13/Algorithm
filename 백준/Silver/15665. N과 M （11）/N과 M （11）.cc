4d318dd68d7ff03e00af65f02bc29cd5033335ee
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2024-08-10 13:22:21 +0900

    [Silver II] Title: N과 M (11), Time: 112 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+
+#define MAX 10001
+using namespace std;
+
+int N, M;
+int arr[7];
+int ans[7];
+
+void Input() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+}
+
+void Backtracking(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	int tmp = 0;
+	for (int i = 0; i < N; i++) {
+		if (tmp == arr[i]) continue;
+
+		ans[cnt] = arr[i];
+		tmp = arr[i];
+		Backtracking(cnt + 1);
+	}
+}
+
+void Solve() {
+	Backtracking(0);
+}
+
+void Solution() {
+	Input();
+	Solve();
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	Solution();
+
+	return 0;
+}
