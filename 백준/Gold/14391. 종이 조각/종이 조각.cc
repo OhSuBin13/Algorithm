504b349855c76dc0da68a3776a7943db2c98257b
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2024-10-21 18:00:20 +0900

    [Gold III] Title: 종이 조각, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14391. 종이 조각/종이 조각.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int N, M;
+int board[4][4];
+
+bool isHorizontal(int r, int c, int mask) {
+	int idx = r * M + c;
+	return ((1 << idx) & mask) != 0;
+}
+
+int calcSum(int mask) {
+	int ret = 0;
+	vector<vector<bool>> visit(N, vector<bool>(M, 0));
+
+	for (int i = 0; i < N * M; i++) {
+		int r = i / M;
+		int c = i % M;
+		int nr = r;
+		int nc = c;
+		int sum = 0;
+
+		while (nr < N && nc < M &&
+			!visit[nr][nc] &&
+			isHorizontal(r, c, mask) == isHorizontal(nr, nc, mask)) {
+			sum *= 10;
+			sum += board[nr][nc];
+			visit[nr][nc] = true;
+
+			if (isHorizontal(r, c, mask)) nc++;
+			else nr++;
+		}
+
+		ret += sum;
+	}
+
+	return ret;
+}
+
+int optimize() {
+	int ret = 0;
+
+	for (int i = 0; i < (1 << N * M); i++) {
+		ret = max(ret, calcSum(i));
+	}
+
+	return ret;
+}
+
+void Input() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		string temp;
+		cin >> temp;
+
+		for (int j = 0; j < M; j++) {
+			board[i][j] = temp[j] - '0';
+		}
+	}
+}
+
+void Solve() {
+	cout << optimize();
+}
+
+void Solution() {
+	Input();
+	Solve();
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	Solution();
+
+	return 0;
+}
