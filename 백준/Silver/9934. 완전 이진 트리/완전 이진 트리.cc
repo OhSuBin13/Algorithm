01b575deea173460985304d7e588ee8949e1bfdf
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2025-10-21 00:34:36 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -1,54 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-
 using namespace std;
 
-int K;
-vector<int> building;
-vector<int> depth[11];
+int node[1024] = { 0, };
+vector<vector<int>> v(11);
 
-void Input() {
-	cin >> K;
-	for (int i = 0; i < pow(2,K) - 1; i++) {
-		int a;
-		cin >> a;
-		building.push_back(a);
-	}
-}
-
-void Inorder(int start, int end, int level) {
+void Inorder(int start, int end, int height) {
+	if (start > end) return;
 	int mid = (start + end) / 2;
-	depth[level].push_back(building[mid]);
-
-	if (start == end) return;
-
-	Inorder(start, mid - 1, level + 1);
-	Inorder(mid + 1, end, level + 1);
-}
-
-void Solve() {
-	Inorder(0, building.size()-1, 1);
-
-	for (int i = 1; i <= K; i++) {
-		for (int j = 0; j < depth[i].size(); j++) {
-			cout << depth[i][j] << " ";
-		}
-		cout << "\n";
-	}
-}
-
-void Solution() {
-	Input();
-	Solve();
+	v[height].push_back(node[mid]);
+	Inorder(start, mid - 1, height + 1);
+	Inorder(mid + 1, end, height + 1);
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	Solution();
-
+	int k, num;
+	cin >> k;
+	int maxNum = pow(2, k) - 1;
+	for (int i = 1; i <= maxNum; i++) {
+		cin >> node[i];
+	}
+	Inorder(1, maxNum, 1);
+	for (int i = 1; i <= k; i++) {
+		for (int j = 0; j < v[i].size(); j++) {
+			cout << v[i][j] << " ";
+		}
+		cout << "\n";
+	}
 	return 0;
 }
