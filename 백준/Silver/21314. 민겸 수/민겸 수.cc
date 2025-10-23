7891f4d34120b9299bf4d7095e02886cd84c1503
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2025-10-23 11:08:57 +0900

    [Silver I] Title: 민겸 수, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.cc
+++ 백준/Silver/21314. 민겸 수/민겸 수.cc
@@ -1,98 +1,47 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 #include <vector>
-#include <queue>
-#include <map>
 #include <cmath>
-
-#define MAX 500000
+#include <string>
 using namespace std;
-
-string s;
-
-void Input() {
-	cin >> s;
-}
-
-string FindMax(string s) {
-	string res = "";
-	int continuous = 0;
-
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] == 'M') {
-			continuous++;
-
-			if (i == s.size() - 1) {
-				string num = "1";
-
-				for (int i = 0; i < continuous - 1; i++) {
-					num += '1';
-				}
-
-				res += num;
-				continuous = 0;
-			}
-		}
-		else {
-			string num = "5";
-
-			for (int i = 0; i < continuous; i++) {
-				num += '0';
-			}
-
-			res += num;
-			continuous = 0;
-		}
-	}
-
-	return res;
-}
-
-string FindMin(string s) {
-	string res = "";
-	int continuous = 0;
-
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] == 'M') {
-			continuous++;
-			
-			if (i == s.size() - 1 || s[i+1] == 'K') {
-				string num = "1";
-
-				for (int i = 0; i < continuous-1; i++) {
-					num += '0';
-				}
-
-				res += num;
-				continuous = 0;
-			}
-		}
-		else {
-			res += '5';
-		}
-	}
-
-
-	return res;
-}
-
-void Solve() {
-	cout << FindMax(s) << "\n";
-	cout << FindMin(s);
-}
-
-void Solution() {
-	Input();
-	Solve();
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	cin.tie(NULL); cout.tie(NULL);
 
-	Solution();
+	string s; cin >> s;
+	int size = s.size();
+	string maxStr = "", minStr = "";
+	int mNum = 0;
 
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'M') mNum++;
+		else {
+			maxStr += '5';
+			for (int i = 0; i < mNum; i++) maxStr += '0';
+			mNum = 0;
+		}
+	}
+	if (mNum != 0) {
+		for (int i = 0; i < mNum; i++) maxStr += '1';
+	}
+	mNum = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'M') mNum++;
+		else {
+			if (mNum) {
+				minStr += '1';
+				for (int i = 0; i < mNum - 1; i++) minStr += '0';
+			}
+			minStr += '5';
+			mNum = 0;
+		}
+	}
+	if (mNum != 0) {
+		minStr += '1';
+		for (int i = 0; i < mNum - 1; i++) minStr += '0';
+	}
+	
+	cout << maxStr << "\n" << minStr << "\n";
 	return 0;
 }
