f6b264035898a7801ae08f15e9df4603c6637808
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2025-11-07 15:28:12 +0900

    [level 2] Title: 전화번호 목록, Time: 81.19 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,25 +1,22 @@
 #include <string>
 #include <vector>
-#include <map>
-
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
-bool solution(vector<string> phone_book) {
+bool solution(vector<string> phoneBook) {
     bool answer = true;
-    map<string, int> phone_map;
-    for (const auto& phone_number : phone_book) {
-		phone_map[phone_number]++;
-	}
-    for (const auto& phone_number : phone_book) {
-        string prefix;
-        for (size_t i = 0; i < phone_number.length() - 1; ++i) {
-            prefix += phone_number[i];
-            if (phone_map.find(prefix) != phone_map.end()) {
-                answer = false;
-                return answer;
-            }
+
+    sort(phoneBook.begin(), phoneBook.end());
+
+    for (int i = 0; i < phoneBook.size() - 1; i++)
+    {
+        if (phoneBook[i] == phoneBook[i + 1].substr(0, phoneBook[i].size()))
+        {
+            answer = false;
+            break;
         }
-	}
+    }
 
     return answer;
 }
