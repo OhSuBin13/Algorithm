8bf5a32f209eacc8cc2ba14a237a3dd4468fbde4
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2025-11-07 16:52:48 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -7,18 +7,14 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
     int answer = 0;
 
-	// Create a map to count the number of clothes for each type
-	unordered_map<string, int> type_count;
-	for (const auto& item : clothes) {
-		type_count[item[1]]++;
-	}
-
-	// Calculate the number of combinations
-	int combinations = 1;
-	for (const auto& pair : type_count) {
-		combinations *= (pair.second + 1);
-	}
-	answer = combinations - 1; // Subtract the case where no clothes are worn
-
+    unordered_map<string, int> type_count;
+    for (auto& item : clothes) {
+        type_count[item[1]]++;
+    }
+    answer = 1;
+    for (auto& type : type_count) {
+		answer *= (type.second + 1);
+    }
+	answer -= 1;
     return answer;
 }
