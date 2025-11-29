a1a64714275b34491c6da96822ad04388f4fd112
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2025-11-29 16:13:55 +0900

    [level 2] Title: 큰 수 만들기, Time: 4.77 ms, Memory: 5.43 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+
+    for (int i = 0; i < number.length(); i++) {
+        while (!answer.empty() && answer.back() < number[i] && k > 0) {
+            k--;
+            answer.pop_back();
+        }
+
+        answer.push_back(number[i]);
+    }
+    if (k > 0) {
+        answer = answer.substr(0, answer.length() - k);
+    }
+    return answer;
+}
