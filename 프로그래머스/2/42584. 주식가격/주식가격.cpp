e7ecdb55c54ff5f57883594d07cb14e32df27128
OhSuBin13 <149787142+OhSuBin13@users.noreply.github.com>
2025-12-27 13:43:31 +0900

    [level 2] Title: 주식가격, Time: 27.18 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,25 +1,27 @@
 #include <string>
 #include <vector>
 #include <stack>
-#include <iostream>
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
-    stack<pair<int, int>> s;
-    answer.resize(prices.size());
-
+    stack<pair<int,int>> s; // price, time
+    int tmp[100000];
     for (int i = 0; i < prices.size(); i++) {
-        while (!s.empty() && s.top().first > prices[i]) {
-            answer[s.top().second] = i - s.top().second;
+        int price = prices[i];
+        while (!s.empty() && s.top().first > price) {
+            tmp[s.top().second] = i - s.top().second;
             s.pop();
         }
-        s.push(make_pair(prices[i], i));
+        s.push(make_pair(price, i));
     }
-
     while (!s.empty()) {
-        answer[s.top().second] = prices.size() - s.top().second - 1;
+        tmp[s.top().second] = prices.size() - 1 - s.top().second;
         s.pop();
+    }
+    for (int i = 0; i < prices.size(); i++) {
+        answer.push_back(tmp[i]);
     }
     return answer;
 }
